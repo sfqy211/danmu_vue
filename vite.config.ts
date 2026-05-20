edc8262ba1cc94778f6288da4fddbd59ba3ed81f
sfqy211 <hangruizhang63@gmail.com>
2026-05-20 13:38:46 +0800

    fix: 修复服务器配置中的主机地址

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { tr } from 'element-plus/es/locales.mjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5200,
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
